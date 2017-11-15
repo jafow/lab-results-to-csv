252a42668ca1f8846f754c5dd9706f9d44bd973b
jafow <jared.a.fowler@gmail.com>
2017-11-14 23:32:04 -0800

    Refactored to write test types as column names:
    Changes the data structures for storing the scraped
    data to use a dict stored on the date as keys.
    The csv module favors dictionaries so makes writing
    nice.

--- main.py
+++ main.py
@@ -19,41 +19,49 @@ def main ():
     html_files = glob.glob('./assets/*.html')
 
     for file in html_files:
-        # open the current file
         f = open(file, 'r')
         input = f.read()
 
-        # read into soup
         soup = BeautifulSoup(input, 'html.parser')
 
-        """data for 'All results' output are kept under the first <section> tag
-        in a unordered list"""
-        d = [c for c in soup.section.ul.children]
+        """ data for 'All results' output are kept under the first <section> tag
+        in a unordered list """
+        nodes = [c for c in soup.section.ul.children]
 
-        levels = [
-                {
-                    'level': node.div.span.text,
-                    'date': format_date(node.find_all('span', class_='date')[0].text)
-                    }
-                for node in d
-                if len(list(node)) > 1
-                ]
+        # get the name of current test TYPE;
+        # these are written as fieldnames/columns on the CSV file
+        measurement_key = (soup.section.dt.h3.text).upper()
+        add_column(res['columns'], measurement_key)
 
-        res[measurement_key] = levels
+        for node in nodes:
+            if len(list(node)) > 1:
+                date_key = node.find_all('span', class_='date')[0].text
+                level = node.div.span.text
+
+                test_data = {measurement_key: level}
+
+                if res.get(date_key) is not None:
+                    # we have records at this date so extend the dict
+                    res[date_key].update(test_data)
+                else:
+                    res[date_key] = test_data
 
     return res
 
-# csv fields: date, measurement_key, level
 def make_csv(data):
     """ write a csv file from a dictionary of {'level': [list of entries]} """
     with open('./assets/levels-entries.csv', 'w', newline='') as csvfile:
-        fieldnames = ['date', 'test', 'level']
+        fieldnames = ['date'] + sorted(data['columns'])
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
         writer.writeheader()
 
         for key in data:
-            for xs in data[key]:
-                writer.writerow({'date': xs['date'], 'test': key, 'level': xs['level']})
+            if key is not 'columns':
+                date = {'date': key}
+                # assign all key & values onto date dict
+                row =  {**date, **data[key]}
+                writer.writerow(row)
+
+    return csvfile
 
 make_csv(main())
